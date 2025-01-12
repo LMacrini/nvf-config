0d25e8f2ce5c32578931b0799040c523ca9faf12
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-01-11 21:12:48 -0500

    replace tabs with spaces

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -3,27 +3,28 @@
 {
   vim = {
     theme = {
-	  enable = true;
-	  name = "catppuccin";
-	  style = "macchiato";
-	};
+      enable = true;
+      name = "catppuccin";
+      style = "macchiato";
+    };
 
     statusline.lualine.enable = true;
     telescope.enable = true;
     autocomplete.nvim-cmp.enable = true;
 
     languages = {
-	  enableLSP = true;
-	  enableTreesitter = true;
+      enableLSP = true;
+      enableTreesitter = true;
 
-	  nix.enable = true;
-	  zig.enable = true;
-	  python.enable = true;
-	  ts.enable = true;
+      nix.enable = true;
+      zig.enable = true;
+      python.enable = true;
+      ts.enable = true;
     };
 
-	filetree.neo-tree = {
-		enable = true;
-	};
+    filetree.neo-tree = {
+      enable = true;
+    };
+
   };
 }
