3d0aed78b7390db86dc01d0c44f00aa2ad5e6352
Lionel Macrini <62901118+LMacrini@users.noreply.github.com>
2025-01-11 20:20:55 -0500

    add support for all systems

--- flake.nix
+++ flake.nix
@@ -4,13 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, nvf }: {
-    packages."x86_64-linux".default =
-      (nvf.lib.neovimConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [ ./nvf-configuration.nix ];
-      }).neovim;
+  outputs = { self, nixpkgs, nvf, systems }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = eachSystem (system: let
+      pkgs = import nixpkgs {system = system;};
+    in {
+      default =
+        (nvf.lib.neovimConfiguration {
+          pkgs = pkgs;
+          modules = [ ./nvf-configuration.nix ];
+        }
+    });
   };
 }
